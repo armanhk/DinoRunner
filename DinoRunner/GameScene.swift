a9b34fb46af725b64c0b77d9d94e9ea56357b3c1
Arman Kazi <merl@Armans-MacBook-Pro.local>
2018-03-22 18:52:10 -0700

    txt input for final pres

--- DinoRunner/GameScene.swift
+++ DinoRunner/GameScene.swift
@@ -9,15 +9,52 @@
 import SpriteKit
 import UIKit
 import GameplayKit
+import Foundation
+
+func readDataFromFile(file:String) -> Array<Any>!{
+    guard let filepath = Bundle.main.path(forResource: file, ofType: "txt")
+        else {
+            return nil
+    }
+    do {
+        let content = try String(contentsOfFile: filepath)
+        let lines : [String] = content.components(separatedBy: "\n")
+        var ints : [Int] = []
+        for var line in lines {
+            if (line == "") { continue }
+            line = line.filter{(!"\r".contains($0))}
+            ints.append(Int(line)!)
+        }
+        return ints
+    } catch {
+        print("File Read Error for file \(filepath)")
+        return nil
+    }
+}
+
+var file1 : [Int] = readDataFromFile(file: "data-at") as! [Int]
+var file2 : [Int] = readDataFromFile(file: "data-bl") as! [Int]
+var i = 0
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
     
+    
+    //MARK: File i/o
+    
+    
     // Score
     var scoreLabel = SKLabelNode()
+    var brainLabel = SKLabelNode()
     
     var score = 0 {
         didSet {
             scoreLabel.text = "Score: \(score)"
+        }
+    }
+    
+    var brainActivity = 0 {
+        didSet {
+            brainLabel.text = "Attn Level: \(brainActivity)"
         }
     }
     
@@ -62,34 +99,58 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         scoreLabel.position = CGPoint(x: size.width-10, y: size.height/7)
         addChild(scoreLabel)
         
+        // Brain activity
+        brainLabel = SKLabelNode(fontNamed: "Chalkduster")
+        brainLabel.fontColor = SKColor.blue
+        brainLabel.text = "Score: "
+        brainLabel.horizontalAlignmentMode = .right
+        brainLabel.position = CGPoint(x: size.width-10, y: size.height/4)
+        addChild(brainLabel)
+        
         run(SKAction.repeatForever(SKAction.sequence([SKAction.wait(forDuration: 2.0), SKAction.run(addEnemy), SKAction.wait(forDuration: 2.0, withRange: 6.0)])))
         
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        //for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
-    func touchDown(atPoint pos: CGPoint) {
-        jump()
+    func touchDown(attn: Int) {
+        if (attn >= 0){
+            jump()
+        }
     }
     
     
     func jump() {
         
         // These two lines prevent double jumps from multitouch
-        self.isUserInteractionEnabled = false
+        
         let enable = SKAction.run { self.isUserInteractionEnabled = true }
         
         // Defines jumping action
         let jumpUpAction = SKAction.moveBy(x: 0, y: 150, duration: 0.5)
         let jumpDownAction = SKAction.moveBy(x: 0, y: -150, duration: 0.5)
         let jumpSequence = SKAction.sequence([jumpUpAction, jumpDownAction, enable])
+        i+=1
         
-        player.run(jumpSequence)
+        if (self.isUserInteractionEnabled == true) {
+            player.run(jumpSequence)
+        }
+        self.isUserInteractionEnabled = false
     }
     
     override func update(_ currentTime: TimeInterval) {
+        if (i < file1.count){
+            if (file1[i] >= 90) {
+                jump()
+            }
+            self.brainActivity = file1[i]
+            i+=1
+        }
+        
+        if (score > 1500) {self.playerEnemyCollision()}
+        
         keepScore()
         // Called before each frame is rendered
     }
@@ -141,7 +202,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     
     // Collision methods
-    func playerEnemyCollision(player: SKSpriteNode, enemy: SKSpriteNode) {
+    func playerEnemyCollision() {
         let gameOver = SKAction.run {
             let reveal = SKTransition.flipHorizontal(withDuration: 0.5)
             let gameOverScene = GameOverScene(size: self.size, score: self.score)
@@ -150,7 +211,9 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         player.run(SKAction.sequence([gameOver]))
     }
     
+    /*
     func didBegin(_ contact: SKPhysicsContact) {
+        
         var firstBody: SKPhysicsBody
         var secondBody: SKPhysicsBody
         if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
@@ -168,5 +231,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         }
         
     }
+ */
  
 }
